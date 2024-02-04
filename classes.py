07a0e23abd935c0351da2eeffcaeab599ea2d7e3
Nuriman <nu_serikuly@kbtu.kz>
2024-02-04 18:18:25 +0600

    update

---
+++ classes.py
@@ -0,0 +1,66 @@
+#example 1
+class StringManipulator:
+    def __init__(self):
+        self.string = ""
+
+    def getString(self):
+        self.string = input("Enter a string: ")
+
+    def printString(self):
+        print(self.string.upper())
+#example 2
+class Shape:
+    def area(self):
+        return 0
+
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+
+    def area(self):
+        return self.length ** 2
+
+#example 3
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+#example 4
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print(f"Point coordinates: ({self.x}, {self.y})")
+
+    def move(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+
+    def dist(self, other_point):
+        distance = math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
+        return distance
+
+#example 5
+
+#example 6
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
